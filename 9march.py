9e5c6bec33d983698a65d0f39b2f90d03609b80f
cknthw9 <bazarsadaev06@mail.ru>
2026-03-11 10:49:36 +0800

    Refactor student management logic in 9march.py

--- 9march.py
+++ 9march.py
@@ -1,36 +1,54 @@
-a=int(input("1. Show students\n2. Add student\n3. Update student\n4. Delete student\n5. Exit\n\n"))
+while True:
 
-if a == 1:
-    with open("data.txt", "r") as file:
-        print(file.read())
+    a=int(input("1. Show students\n2. Add student\n3. Update student\n4. Delete student\n5. Exit\n\n"))
 
-elif a == 2:
-    with open("data.txt", "a") as file:
-        name = input("Name:")
-        age = input("Age:")
-        file.write(f"\n{name + " " + age}")
+    if a == 1:
+        with open("data.txt", "r") as file:
+            print(file.read())
+
+    elif a == 2:
+        with open("data.txt", "a") as file:
+            name = input("Name:")
+            age = input("Age:")
+            file.write(f"{name + "," + age}\n")
+            
+
+    elif a == 3:
+        updated_data=[]
+
+        v = input("Isert the name that you want to change: ")
+        c=input("Name: " )
+        age=input("Age: ")
+
+        with open("data.txt", "r") as file:
+            lines = file.readlines()
+
+            for line in lines:
+                name = line.split()[0]
+
+                if name == v:
+                    updated_data.append(f"{c +","+ age}\n")
+                else:
+                    updated_data.append(line)
         
+        with open("data.txt", "w") as file:
+            file.writelines(updated_data)
 
-elif a == 3:
-    updated_data=[]
+    elif a == 4:
 
-    v = input("Isert the name that you want to change: ")
-    c=input("Name: " )
-    age=input("Age: ")
+        nn=input("Who do you want to delete: ")
 
-    with open("data.txt", "r") as file:
-        lines = file.readlines()
+        with open("data.txt", "r") as file:
+            lines=file.readlines()
+            
 
-        for line in lines:
-            name= line.split()[0]
+        with open("data.txt", "w") as file:
+            for line in lines:
+                dn = line.strip().split(",")
+                if dn[0] != (nn):
+                    file.write(line)
 
-            if name == v:
-                updated_data.append(c +" "+ age)
-            else:
-                updated_data.append(line)
-    
-    with open("data.txt", "w") as file:
-        file.writelines(updated_data)
-
+    elif a == 5:
+        break
 
 
